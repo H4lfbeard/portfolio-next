2de71f8a3ca23665e2270437594b115590f0de93
H4lfbeard <humbert.thomas.pro@gmail.com>
2025-09-15 16:44:42 +0200

    Initial commit

---
+++ app/components/Header.jsx
@@ -0,0 +1,44 @@
+import { assets } from "@/assets/assets";
+import Image from "next/image";
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col items-center justify-center gap-4">
+      <div>
+        <Image src={assets.profile_img} alt="" className="rounded-full w-32" />
+      </div>
+      <h3 className="flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo">
+        Hi ! I'm Thomas HUMBERT
+        <Image src={assets.hand_icon} alt="" className="w-6" />
+      </h3>
+      <h1 className="text-3xl sm:test-6xl lg:text-[66px] font-Ovo">
+        Designer and integrator Web based in Bordeaux
+      </h1>
+      <p className="max-w-2xl mx-auto font-Ovo ">
+        I am a designer and integrator web from Annecy now based in Bordeaux
+        with more than 7 years of experience. I just recently followed a 6
+        months React JS bootcamp in order to improve my web knowledge.
+      </p>
+      <div className="flex flex-col sm:flex-row items-center gap-4 mt-4">
+        <a
+          href="#contact"
+          className="px-10 py-3 border rounded-full border-gray-500 bg-black text-white flex items-center gap-2"
+        >
+          contact me
+          <Image src={assets.right_arrow_white} alt="" className="w-4" />
+        </a>
+        <a
+          href="/sample-resume.pdf"
+          download
+          className="px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2"
+        >
+          My resume
+          <Image src={assets.download_icon} alt="" className="w-4" />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Header;

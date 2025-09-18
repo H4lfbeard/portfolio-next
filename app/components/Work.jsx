0cbff8702a6c75e765150c0906fea83b41966b54
H4lfbeard <humbert.thomas.pro@gmail.com>
2025-09-18 16:07:49 +0200

    work section

---
+++ app/components/Work.jsx
@@ -0,0 +1,49 @@
+import { assets, workData } from "@/assets/assets";
+import React from "react";
+import Image from "next/image";
+
+export const Work = () => {
+  return (
+    <div id="work" className="w-full px-[12%] py-10 scroll-mt-20">
+      <h4 className="text-center mb-2 text-lg font-Ovo">Mon portfolio</h4>
+      <h2 className=" text-center text-5xl font-Ovo">Mes derniers projets</h2>
+      <p className="text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo">
+        Bienvenue sur mon portfolio de designer et intégrateur web. Ici vous
+        pouvez visiter une collections de mes derniers projets mettant en avant
+        mon expertise en design et intégration web.
+      </p>
+
+      <ul className="grid grid-cols-services gap-5 my-10">
+        {workData.map((project, index) => (
+          <li
+            key={index}
+            style={{ backgroundImage: `url(${project.bgImage})` }}
+            className="aspect-square bg-no-repeat bg-center bg-cover border relative group border-gray-400 rounded-lg px-8 py-12 cursor-pointer hover:shadow-black hover:-translate-y-1 duration-500"
+          >
+            <article className="bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7">
+              <div>
+                <h2 className="font-semibold">{project.title}</h2>
+                <p className="text-sm text-gray-700">{project.description}</p>
+              </div>
+              <div className="border rounded-full border-black w-9 aspect-square flex items-center justify-center shadow-[2px_2px_0_#000] group-hover:bg-lime-300 transition">
+                <Image src={assets.send_icon} alt="sent icon" className="w-5" />
+              </div>
+            </article>
+          </li>
+        ))}
+      </ul>
+
+      <a
+        href=""
+        className="w-max flex items-center justify-center gap-2 text-gray-700 border-[0.5px] border-gray-700 rounded-full py-3 px-10 mx-auto my-20 hover:bg-lightHover duration-500"
+      >
+        Show more{" "}
+        <Image
+          src={assets.right_arrow_bold}
+          alt="right arrow"
+          className="w-4"
+        />
+      </a>
+    </div>
+  );
+};
